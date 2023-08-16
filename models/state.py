1d66da2d559f22418ebe88e57e7ae4f4d8e21f08
hug0-cstrs <hugo.casteras@outlook.fr>
2023-08-16 12:12:15 +0200

    update file

--- models/state.py
+++ models/state.py
@@ -20,10 +20,7 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
     cities = relationship('City', cascade='all, delete', backref='state')
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-    if getenv('HBNB_TYPE_STORAGE', '') != 'db':
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
         def cities(self):
             """
